52baaa6e44386c8daae4cb80e19bdd9f0def2565
dongie <1412834290@qq.com>
2022-06-19 21:20:18 +0800

    init

---
+++ src/main/java/com/example/feeddemo/FeedDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.feeddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/feeddemo/FeedDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feeddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
