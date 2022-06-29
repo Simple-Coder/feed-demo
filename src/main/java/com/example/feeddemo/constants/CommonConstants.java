b3f31d288f64bcd5ff2011401c2fdeef267c8d61
dongie <1412834290@qq.com>
2022-06-29 21:53:17 +0800

    常量增加

--- src/main/java/com/example/feeddemo/constants/CommonConstants.java
+++ src/main/java/com/example/feeddemo/constants/CommonConstants.java
@@ -7,4 +7,12 @@ package com.example.feeddemo.constants;
 public interface CommonConstants {
     int SUCCESS = 200;
     int FAIL = 500;
+
+
+    int BIG_V = 1;
+    int COMMON_USER = 0;
+
+
+    int FEED_STATUS_NORMAL = 1;
+    int FEED_STATUS_DEL = -1;
 }
