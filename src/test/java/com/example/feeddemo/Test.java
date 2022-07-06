fa28bd60298fd8ada7d7e106dbb6fd036270f471
dongie <1412834290@qq.com>
2022-07-06 21:07:41 +0800

    test

--- src/test/java/com/example/feeddemo/Test.java
+++ src/test/java/com/example/feeddemo/Test.java
@@ -2,9 +2,31 @@ package com.example.feeddemo;
 
 import org.jasypt.util.text.BasicTextEncryptor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Test {
     //给配置文件加密
     public static void main(String[] args) {
+        Long currentUserId = 2L;
+        List<Long> userIds = new ArrayList<>();
+        userIds.add(1L);
+        userIds.add(2L);
+        userIds.add(3L);
+        userIds.add(4L);
+        userIds.add(null);
+        List<Long> collect1 = userIds.stream().filter(id -> id != null).collect(Collectors.toList());
+        boolean querySelf = false;
+
+        if (!querySelf) {
+            userIds.remove(currentUserId);
+        }
+
+        List<Long> collect = userIds.stream().filter(userId -> (userId != currentUserId)).collect(Collectors.toList());
+
+        System.out.println();
+
 
         // 加密
         BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
